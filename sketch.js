751303202caeab296466aee5bac1f8a297ba5b9b
Aman-whitehatjr <85725325+Aman-whitehatjr@users.noreply.github.com>
2021-08-26 14:57:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var student1
+function setup() {
+  createCanvas(400,400);
+
+student1=new Student("Aman", 12, 7)
+student1.display()
+
+}
+
+function draw() 
+{
+    background(30);
+
+    //student1.display()
+
+
+}
---
+++ student.js
@@ -0,0 +1,25 @@
+
+class Student
+{
+   constructor(name,age,grade){
+
+this.age=age
+this.grade=grade
+this.name=name
+
+
+   }
+display(){
+
+console.log(this.name)
+console.log(this.age)
+console.log(this.grade)
+
+}
+
+
+
+}
+
+
+
